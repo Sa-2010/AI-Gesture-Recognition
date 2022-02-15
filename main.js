7528413bffed7bb884bb1607af63f0510255ae4b
Sarah <75193877+Sa-2010@users.noreply.github.com>
2022-02-15 17:03:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
  video = createCapture(VIDEO);
  video.size(550,500);
@@ -9,7 +15,11 @@ function setup() {
 }
 
 function draw() {
-  background('#e84396')
+  background('#e84396');
+ document.getElementById("square_side").innerHtml = "Width And Height of a Square will be = " + differnce + "px";
+ fill('#c906c3');
+ stroke('#50c7fa');
+ square(noseX, noseY, difference);
 }
 
 function modelLoaded() {
@@ -21,5 +31,12 @@ function gotPoses(results)
   if(results.lenght > 0)
   {
     console.log(results);
+  noseX = results[0].pose.nose.x;
+  noseY = results[0].pose.nose.y;
+  console.log("noseX =" + noseX +"noseY = " + noseY);
+  rightWristX = results[0].pose.rightWrist.x;
+  leftWristX = results[0].pose.leftWrist.x;
+  difference = floor(leftWristX - rightWristX);
+  console.log("leftWristX =" + leftWristX + " rightWristX ="  + rightWristX + "diffrence = " + diffrence);
   }
 }
